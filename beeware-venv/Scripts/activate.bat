2890d2962e82aca79245561a0bd466a0e8159f5a
dewnielc <dewnielc@gmail.com>
2022-04-25 20:14:58 -0400

    my home computer

--- beeware-venv/Scripts/activate.bat
+++ beeware-venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=H:\Github\Repo\AnimalID\beeware-venv
+set VIRTUAL_ENV=C:\Users\danie\Documents\GitHub\AnimalID\beeware-venv
 
 if not defined PROMPT set PROMPT=$P$G
 
